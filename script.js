3aa6ab1c7be6c52a0a6854ec738b86171dc592aa
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-01-21 20:17:26 +0000

    saving user input to local storage

--- script.js
+++ script.js
@@ -91,19 +91,12 @@ function colorCodeSchedule() {
 // Call function ColorCodeSchedule
 colorCodeSchedule();
 
+// Saving users input to local storage when user click save button
 
-/* ```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down 
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-*/
+$(".saveBtn").on("click", function () {
+  // Storing user input from changes made to parent html "id" attribute and sibbling "description" attribute
+  var time = $(this).parent().attr("id");
+  var text = $(this).siblings(".description").val();
+  //save items in local storage.
+  localStorage.setItem(time, text);
+});
