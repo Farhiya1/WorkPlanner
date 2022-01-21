6c8ab257940d7e79bd7dab60e6e122901e5678fd
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-01-21 20:15:49 +0000

    color code time block based on current time status

--- script.js
+++ script.js
@@ -12,6 +12,81 @@ console.log(currentTime);
 var dateOfToday = moment().format("dddd, MMM Do YYYY, h:mm A");
 $("#currentDay").html(dateOfToday);
 
+// Color coding time blocks on planner to indicate whether it is in the past, present, or future
+
+function colorCodeSchedule() {
+  if (currentTime > 8) {
+    $("#hour8").addClass("past");
+  } else if (currentTime >= 8 && currentTime < 9) {
+    $("#hour8").addClass("present");
+  } else if (currentTime < 9) {
+    $("#hour8").addClass("future");
+  }
+  if (currentTime > 9) {
+    $("#hour9").addClass("past");
+  } else if (currentTime >= 9 && currentTime < 10) {
+    $("#hour9").addClass("present");
+  } else if (currentTime < 10) {
+    $("#hour9").addClass("future");
+  }
+  if (currentTime > 10) {
+    $("#hour10").addClass("past");
+  } else if (currentTime >= 10 && currentTime < 11) {
+    $("#hour10").addClass("present");
+  } else if (currentTime < 11) {
+    $("#hour10").addClass("future");
+  }
+  if (currentTime > 11) {
+    $("#hour11").addClass("past");
+  } else if (currentTime >= 11 && currentTime < 12) {
+    $("#hour11").addClass("present");
+  } else if (currentTime < 12) {
+    $("#hour11").addClass("future");
+  }
+  if (currentTime > 12) {
+    $("#hour12").addClass("past");
+  } else if (currentTime >= 12 && currentTime < 13) {
+    $("#hour12").addClass("present");
+  } else if (currentTime < 13) {
+    $("#hour12").addClass("future");
+  }
+  if (currentTime > 13) {
+    $("#hour13").addClass("past");
+  } else if (currentTime >= 13 && currentTime < 14) {
+    $("#hour13").addClass("present");
+  } else if (currentTime < 14) {
+    $("#hour13").addClass("future");
+  }
+  if (currentTime > 14) {
+    $("#hour14").addClass("past");
+  } else if (currentTime >= 14 && currentTime < 15) {
+    $("#hour14").addClass("present");
+  } else if (currentTime < 15) {
+    $("#hour14").addClass("future");
+  }
+  if (currentTime > 15) {
+    $("#hour15").addClass("past");
+  } else if (currentTime >= 15 && currentTime < 16) {
+    $("#hour15").addClass("present");
+  } else if (currentTime < 16) {
+    $("#hour15").addClass("future");
+  }
+  if (currentTime > 16) {
+    $("#hour16").addClass("past");
+  } else if (currentTime >= 16 && currentTime < 17) {
+    $("#hour16").addClass("present");
+  } else if (currentTime < 17) {
+    $("#hour16").addClass("future");
+  }
+  if (currentTime > 17) {
+    $("#hour17").addClass("past");
+  } else if (currentTime >= 17 && currentTime < 18) {
+    $("#hour17").addClass("present");
+  } else if (currentTime < 18) {
+    $("#hour17").addClass("future");
+  }
+}
+
 
 
 
