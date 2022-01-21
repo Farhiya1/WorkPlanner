a677cb9d5ff2202d8538bd68e23fd1e2147f2fed
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-01-21 20:18:41 +0000

    retreiving user input from local storage

--- script.js
+++ script.js
@@ -100,3 +100,16 @@ $(".saveBtn").on("click", function () {
   //save items in local storage.
   localStorage.setItem(time, text);
 });
+
+//Retreive users input from local storage and have the saved events persist on the page when the page is refreshed.
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
